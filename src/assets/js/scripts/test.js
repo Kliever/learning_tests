7be8fccee534a13674de8ed0e3c068f0723b993c
Kliever <mgrenady@gmail.com>
2023-09-30 20:43:08 +0300

    start of administrator development

---
+++ src/assets/js/scripts/test.js
@@ -0,0 +1,21 @@
+// const firstCheck = false;
+// const secondCheck = false;
+// const access = firstCheck
+//   ? "Доступ запрещён"
+//   : secondCheck
+//   ? "Доступ запрещён"
+//   : "Доступ разрешён";
+
+
+
+// const firstCheck = false;
+// const secondCheck = false;
+// let access; 
+
+// if (firstCheck) {
+//   access = "Доступ запрещён";
+// } else if (secondCheck) {
+//   access = "Доступ запрещён";
+// } else {
+//   access = "Доступ разрешён";
+// }
